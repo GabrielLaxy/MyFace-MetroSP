d852b403d5ebc026f7f636dd8b3fe8ee138eb3eb
Gabriel Laxy <gablaxycarreira@gmail.com>
2024-10-24 21:26:33 -0300

    arrumando vizualizacao da tela com o teclado

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
-import { TextInput, Button, IconButton } from 'react-native-paper';
+import {
+	View,
+	Text,
+	Image,
+	TouchableOpacity,
+	Linking,
+	ScrollView,
+	KeyboardAvoidingView,
+	Platform,
+} from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import theme from '../styles/theme';
@@ -24,7 +33,7 @@ interface LoginProps {
 	navigation: LoginScreenNavigationProp;
 }
 
-export default function Login({ navigation }:LoginProps) {
+export default function Login({ navigation }: LoginProps) {
 	const [funcional, setFuncional] = useState('');
 	const [senha, setSenha] = useState('');
 	const [mostrarSenha, setMostrarSenha] = useState(false);
@@ -41,62 +50,72 @@ export default function Login({ navigation }:LoginProps) {
 	return (
 		<SafeAreaView style={styles.container}>
 			<StatusBar animated={true} style="auto" />
-			<View style={styles.logoContainer}>
-				<Image source={logo} style={styles.logo} />
-			</View>
-			<View style={styles.mascotContainer}>
-				<Image source={mascote} style={styles.mascote} />
-			</View>
-			<View style={styles.middleContainer}>
-				<View style={styles.textContainer}>
-					<Text style={styles.title}>Entre na sua conta</Text>
-					<Text style={styles.subtitle}>Bem vindo ao sistema!</Text>
-				</View>
-				<TextInput
-					label="Funcional"
-					value={funcional}
-					onChangeText={text => setFuncional(text)}
-					style={styles.input}
-					mode="outlined"
-					theme={theme}
-				/>
-				<TextInput
-					label="Senha"
-					value={senha}
-					onChangeText={text => setSenha(text)}
-					style={styles.input}
-					secureTextEntry={!mostrarSenha}
-					mode="outlined"
-					right={
-						<TextInput.Icon
-							icon={mostrarSenha ? 'eye-off' : 'eye'}
-							onPress={() => setMostrarSenha(!mostrarSenha)}
+			<KeyboardAvoidingView
+				style={{ flex: 1 }}
+				behavior="padding"
+				keyboardVerticalOffset={Platform.OS === 'ios' ? 100 : 0}
+			>
+				<ScrollView contentContainerStyle={{ flexGrow: 1 }} bounces={false}>
+					<View style={styles.logoContainer}>
+						<Image source={logo} style={styles.logo} />
+					</View>
+					<View style={styles.mascotContainer}>
+						<Image source={mascote} style={styles.mascote} />
+					</View>
+					<View style={styles.middleContainer}>
+						<View style={styles.textContainer}>
+							<Text style={styles.title}>Entre na sua conta</Text>
+							<Text style={styles.subtitle}>Bem vindo ao sistema!</Text>
+						</View>
+						<TextInput
+							label="Funcional"
+							value={funcional}
+							onChangeText={text => setFuncional(text)}
+							style={styles.input}
+							mode="outlined"
+							theme={theme}
 						/>
-					}
-					theme={theme}
-				/>
-				<TouchableOpacity onPress={() => handleMailto('Esqueci minha senha')}>
-					<Text style={styles.forgotPw}>Esqueci minha senha</Text>
-				</TouchableOpacity>
-				<Button
-					mode="contained"
-					onPress={() => {
-						handleLogin(); 
-						navigation.navigate('Cadastro'); 
-					}}
-					style={styles.button}
-					labelStyle={styles.buttonLabel}
-				>
-					Entrar
-				</Button>
-			</View>
-			<View style={styles.bottomContainer}>
-				<TouchableOpacity
-					onPress={() => handleMailto('Política e termos de privacidade')}
-				>
-					<Text style={styles.link}>Política e termos de privacidade</Text>
-				</TouchableOpacity>
-			</View>
+						<TextInput
+							label="Senha"
+							value={senha}
+							onChangeText={text => setSenha(text)}
+							style={styles.input}
+							secureTextEntry={!mostrarSenha}
+							mode="outlined"
+							right={
+								<TextInput.Icon
+									icon={mostrarSenha ? 'eye-off' : 'eye'}
+									onPress={() => setMostrarSenha(!mostrarSenha)}
+								/>
+							}
+							theme={theme}
+						/>
+						<TouchableOpacity
+							onPress={() => handleMailto('Esqueci minha senha')}
+						>
+							<Text style={styles.forgotPw}>Esqueci minha senha</Text>
+						</TouchableOpacity>
+						<Button
+							mode="contained"
+							onPress={() => {
+								handleLogin();
+								navigation.navigate('Cadastro');
+							}}
+							style={styles.button}
+							labelStyle={styles.buttonLabel}
+						>
+							Entrar
+						</Button>
+					</View>
+					<View style={styles.bottomContainer}>
+						<TouchableOpacity
+							onPress={() => handleMailto('Política e termos de privacidade')}
+						>
+							<Text style={styles.link}>Política e termos de privacidade</Text>
+						</TouchableOpacity>
+					</View>
+				</ScrollView>
+			</KeyboardAvoidingView>
 		</SafeAreaView>
 	);
 }
